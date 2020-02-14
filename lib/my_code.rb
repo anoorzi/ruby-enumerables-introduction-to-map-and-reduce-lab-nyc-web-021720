ecf9c1f85dc0e0d0e4272e8395332a47e2d69ac1
Abubaker Noorzi <anoorzi123@gmail.com>
2020-02-14 13:50:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -34,7 +34,7 @@ def map_to_square(array)
 end
 
 
-def reduce_to_total(array, start = 0)
+def reduce_to_total(source_array, starting_point = 0)
   count = start
   total = 0
   while count < array.length do
