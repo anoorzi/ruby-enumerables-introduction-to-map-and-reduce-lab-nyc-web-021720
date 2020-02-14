41bae0fd99b9aa678ea2928e6a75d3c9a34f0117
Abubaker Noorzi <anoorzi123@gmail.com>
2020-02-14 13:34:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,7 +5,19 @@ def map_to_negativize(array)
     array[count]=array[count]*-1
     count +=1
   end
-
  array
+end
 
+
+def map_to_no_change(array)
+  array
+end
+
+
+def map_to_double(array)
+  count = 0
+  while count < array.length do
+    array[count]=array[count]*2
+    count +=1
+  end
 end
